8480c32253ccbcc736b90a07728446b81cd0ac66
Cristhian Parra <cdparra@gmail.com>
2015-10-10 17:15:36 -0700

    Added encryption on passwords

--- app/providers/MyLoginUsernamePasswordAuthUser.java
+++ app/providers/MyLoginUsernamePasswordAuthUser.java
@@ -1,5 +1,7 @@
 package providers;
 
+import org.mindrot.jbcrypt.BCrypt;
+
 import com.feth.play.module.pa.providers.password.DefaultUsernamePasswordAuthUser;
 
 public class MyLoginUsernamePasswordAuthUser extends
@@ -36,5 +38,23 @@ public class MyLoginUsernamePasswordAuthUser extends
 	public long expires() {
 		return expiration;
 	}
-
+	
+	/**
+	 * You *SHOULD* provide your own implementation of this which implements your own security.
+	 */
+	@Override
+	protected String createPassword(final String clearString) {
+		return BCrypt.hashpw(clearString, BCrypt.gensalt());
+	}
+	
+	/**
+	 * You *SHOULD* provide your own implementation of this which implements your own security.
+	 */
+	@Override
+	public boolean checkPassword(final String hashed, final String candidate) {
+		if(hashed == null || candidate == null) {
+			return false;
+		}
+		return BCrypt.checkpw(candidate, hashed);
+	}
 }
