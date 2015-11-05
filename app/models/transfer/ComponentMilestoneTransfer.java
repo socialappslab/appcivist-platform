d325356f26485616349beb6bb09707db568ce24c
Cristhian Parra <cdparra@gmail.com>
2015-11-04 22:43:10 -0800

    Starting version of new transfer intermediate models

---
+++ app/models/transfer/ComponentMilestoneTransfer.java
@@ -0,0 +1,102 @@
+package models.transfer;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import enums.ContributionTypes;
+
+public class ComponentMilestoneTransfer {
+	private Long milestoneId;
+	private String title;
+	private int position;
+	private String description;
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm a z")
+	private Date start = Calendar.getInstance().getTime(); 
+	private Integer days = 1; 
+	private UUID uuid = UUID.randomUUID();
+	private String uuidAsString;
+	@Enumerated(EnumType.STRING)
+	private ContributionTypes mainContributionType = ContributionTypes.BRAINSTORMING;
+
+	public ComponentMilestoneTransfer() {
+		super();
+	}
+	
+	public Long getMilestoneId() {
+		return milestoneId;
+	}
+
+	public void setMilestoneId(Long milestoneId) {
+		this.milestoneId = milestoneId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Date getStart() {
+		return start;
+	}
+
+	public void setStart(Date start) {
+		this.start = start;
+	}
+
+	public Integer getDays() {
+		return days;
+	}
+
+	public void setDays(Integer days) {
+		this.days = days;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getUuidAsString() {
+		return uuidAsString;
+	}
+
+	public void setUuidAsString(String uuidAsString) {
+		this.uuidAsString = uuidAsString;
+	}
+
+	public ContributionTypes getMainContributionType() {
+		return mainContributionType;
+	}
+
+	public void setMainContributionType(ContributionTypes mainContributionType) {
+		this.mainContributionType = mainContributionType;
+	}
+}
---
+++ app/models/transfer/ContributionTransfer.java
@@ -0,0 +1,292 @@
+package models.transfer;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
+import models.ComponentInstanceMilestone;
+import models.Contribution;
+import models.Hashtag;
+import models.Resource;
+import models.Theme;
+import models.WorkingGroup;
+import models.location.Location;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import enums.ContributionTypes;
+
+public class ContributionTransfer {
+	private Long contributionId;
+	private UUID uuid = UUID.randomUUID();
+	private String uuidAsString;
+	private String title;
+	private String text;
+	@Enumerated(EnumType.STRING)
+	private ContributionTypes type;
+	private String textIndex;
+	private Location location;
+	private String budget;
+	@JsonIgnoreProperties({ "providers", "roles", "permissions", "sessionKey", "identifier"})
+	private List<UserTransfer> authors = new ArrayList<>();
+	@JsonIgnoreProperties({ "supportedMembership", "managementType",
+			"resources", "forum", "containingSpaces", "themes", "configs",
+			"forumPosts", "brainstormingContributions", "proposals" })
+	private List<WorkingGroupTransfer> workingGroupAuthors = new ArrayList<>();
+	private ContributionStatisticsTransfer stats;
+	private List<Theme> themes;
+	private List<Resource> attachments;
+	private List<Hashtag> hashtags = new ArrayList<Hashtag>();
+	private List<Contribution> comments = new ArrayList<Contribution>();
+	private List<ComponentInstanceMilestone> associatedMilestones = new ArrayList<ComponentInstanceMilestone>();
+
+	/* 
+	 * Fields specific to the type ACTION_ITEM
+	 */
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm a z")
+	private Date actionDueDate; 
+	private Boolean actionDone = false;
+	private String action; 
+	private String assessmentSummary;
+	
+	// Fields specific to the type PROPOSAL and ASSESSMENT 
+	private Resource extendedTextPad;
+
+	// Fields specific to the type PROPOSAL
+	private List<Contribution> assessments;
+	
+	private List<WorkingGroup> responsibleWorkingGroups;
+	private List<Hashtag> existingHashtags;
+	private List<WorkingGroupTransfer> existingResponsibleWorkingGroups;
+	private List<ContributionTransfer> existingContributions;
+	
+	public ContributionTransfer() {
+		super();
+	}
+
+	public Long getContributionId() {
+		return contributionId;
+	}
+
+	public void setContributionId(Long contributionId) {
+		this.contributionId = contributionId;
+	}
+
+	public UUID getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getUuidAsString() {
+		return uuidAsString;
+	}
+
+	public void setUuidAsString(String uuidAsString) {
+		this.uuidAsString = uuidAsString;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public ContributionTypes getType() {
+		return type;
+	}
+
+	public void setType(ContributionTypes type) {
+		this.type = type;
+	}
+
+	public String getTextIndex() {
+		return textIndex;
+	}
+
+	public void setTextIndex(String textIndex) {
+		this.textIndex = textIndex;
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public void setLocation(Location location) {
+		this.location = location;
+	}
+
+	public String getBudget() {
+		return budget;
+	}
+
+	public void setBudget(String budget) {
+		this.budget = budget;
+	}
+
+	public List<UserTransfer> getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(List<UserTransfer> authors) {
+		this.authors = authors;
+	}
+
+	public List<WorkingGroupTransfer> getWorkingGroupAuthors() {
+		return workingGroupAuthors;
+	}
+
+	public void setWorkingGroupAuthors(
+			List<WorkingGroupTransfer> workingGroupAuthors) {
+		this.workingGroupAuthors = workingGroupAuthors;
+	}
+
+	public ContributionStatisticsTransfer getStats() {
+		return stats;
+	}
+
+	public void setStats(ContributionStatisticsTransfer stats) {
+		this.stats = stats;
+	}
+
+	public List<Theme> getThemes() {
+		return themes;
+	}
+
+	public void setThemes(List<Theme> themes) {
+		this.themes = themes;
+	}
+
+	public List<Resource> getAttachments() {
+		return attachments;
+	}
+
+	public void setAttachments(List<Resource> attachments) {
+		this.attachments = attachments;
+	}
+
+	public List<Hashtag> getHashtags() {
+		return hashtags;
+	}
+
+	public void setHashtags(List<Hashtag> hashtags) {
+		this.hashtags = hashtags;
+	}
+
+	public List<Contribution> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Contribution> comments) {
+		this.comments = comments;
+	}
+
+	public List<ComponentInstanceMilestone> getAssociatedMilestones() {
+		return associatedMilestones;
+	}
+
+	public void setAssociatedMilestones(
+			List<ComponentInstanceMilestone> associatedMilestones) {
+		this.associatedMilestones = associatedMilestones;
+	}
+
+	public Date getActionDueDate() {
+		return actionDueDate;
+	}
+
+	public void setActionDueDate(Date actionDueDate) {
+		this.actionDueDate = actionDueDate;
+	}
+
+	public Boolean getActionDone() {
+		return actionDone;
+	}
+
+	public void setActionDone(Boolean actionDone) {
+		this.actionDone = actionDone;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public void setAction(String action) {
+		this.action = action;
+	}
+
+	public String getAssessmentSummary() {
+		return assessmentSummary;
+	}
+
+	public void setAssessmentSummary(String assessmentSummary) {
+		this.assessmentSummary = assessmentSummary;
+	}
+
+	public Resource getExtendedTextPad() {
+		return extendedTextPad;
+	}
+
+	public void setExtendedTextPad(Resource extendedTextPad) {
+		this.extendedTextPad = extendedTextPad;
+	}
+
+	public List<Contribution> getAssessments() {
+		return assessments;
+	}
+
+	public void setAssessments(List<Contribution> assessments) {
+		this.assessments = assessments;
+	}
+
+	public List<WorkingGroup> getResponsibleWorkingGroups() {
+		return responsibleWorkingGroups;
+	}
+
+	public void setResponsibleWorkingGroups(
+			List<WorkingGroup> responsibleWorkingGroups) {
+		this.responsibleWorkingGroups = responsibleWorkingGroups;
+	}
+
+	public List<Hashtag> getExistingHashtags() {
+		return existingHashtags;
+	}
+
+	public void setExistingHashtags(List<Hashtag> existingHashtags) {
+		this.existingHashtags = existingHashtags;
+	}
+
+	public List<WorkingGroupTransfer> getExistingResponsibleWorkingGroups() {
+		return existingResponsibleWorkingGroups;
+	}
+
+	public void setExistingResponsibleWorkingGroups(
+			List<WorkingGroupTransfer> existingResponsibleWorkingGroups) {
+		this.existingResponsibleWorkingGroups = existingResponsibleWorkingGroups;
+	}
+
+	public List<ContributionTransfer> getExistingContributions() {
+		return existingContributions;
+	}
+
+	public void setExistingContributions(
+			List<ContributionTransfer> existingContributions) {
+		this.existingContributions = existingContributions;
+	}
+}
