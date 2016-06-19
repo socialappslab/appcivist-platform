2c016f886454027e254086b0aaf868e78b1ac87a
Cristhian Parra <cdparra@gmail.com>
2016-06-19 00:12:54 -0700

    Coordinator of Assembly rule considers open assemblies

--- app/security/AssemblyDynamicResourceHandler.java
+++ app/security/AssemblyDynamicResourceHandler.java
@@ -2,6 +2,8 @@ package security;
 
 import java.util.List;
 
+
+import models.Assembly;
 import models.Membership;
 import models.MembershipAssembly;
 import models.MembershipInvitation;
@@ -13,6 +15,7 @@ import play.mvc.Http.Context;
 import be.objectify.deadbolt.core.DeadboltAnalyzer;
 import be.objectify.deadbolt.java.AbstractDynamicResourceHandler;
 import be.objectify.deadbolt.java.DeadboltHandler;
+import enums.ManagementTypes;
 import enums.MembershipTypes;
 import enums.MyRoles;
 
@@ -37,20 +40,21 @@ public class AssemblyDynamicResourceHandler extends AbstractDynamicResourceHandl
                                            if (u!=null) u.setSessionLanguage();
                                            String path = context.request().path();
                                            Long assemblyId = MyDynamicResourceHandler.getIdFromPath(path, resource);
+                                           Assembly a = Assembly.read(assemblyId);
                                            Logger.debug("Checking membership of User in "+resource+"...");
                                            Logger.debug("--> userName = " + u.getUsername());
                                            Logger.debug("--> assemblyId= " + assemblyId);
                                            Membership m = MembershipAssembly.findByUserAndAssemblyIds(u.getUserId(), assemblyId);
-
-                                           if (m!=null && rule.equals("CoordinatorOfAssembly")) {
+                                           Boolean assemblyNotOpen = !a.getProfile().getManagementType().equals(ManagementTypes.OPEN);
+                                           if (m!=null && rule.equals("CoordinatorOfAssembly") && assemblyNotOpen) {
                                                Logger.debug("--> Checking if user is Coordinator");
                                                List<SecurityRole> membershipRoles = m.filterByRoleName(MyRoles.COORDINATOR.getName());
-                                               allowed[0] = membershipRoles != null && !membershipRoles.isEmpty();
-                                           } else if (m!=null && rule.equals("AssemblyMemberIsExpert")) {
+                                               allowed[0] = membershipRoles != null && !membershipRoles.isEmpty(); 
+                                           } else if (m!=null && rule.equals("AssemblyMemberIsExpert") && assemblyNotOpen) {
                                                Logger.debug("--> Checking if user is Expert");
                                                List<SecurityRole> membershipRoles = m.filterByRoleName(MyRoles.EXPERT.getName());
                                                allowed[0] = membershipRoles != null && !membershipRoles.isEmpty();
-                                           } else if (m!=null && rule.equals("ModeratorOfAssembly")) {
+                                           } else if (m!=null && rule.equals("ModeratorOfAssembly") && assemblyNotOpen) {
                                                Logger.debug("--> Checking if user is Moderator");
                                                List<SecurityRole> membershipRoles = m.filterByRoleName(MyRoles.MODERATOR.getName());
                                                allowed[0] = membershipRoles != null && !membershipRoles.isEmpty();                                           
