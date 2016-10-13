4dd681ad385d0cff0e90dafdab5d53302d2a3b12
Cristhian Parra <cdparra@gmail.com>
2016-10-12 19:51:23 -0700

    Updated OnlyMeAndAdmin Authorization Rule for checking user memberships

--- app/controllers/Memberships.java
+++ app/controllers/Memberships.java
@@ -138,6 +138,7 @@ public class Memberships extends Controller {
 			@ApiImplicitParam(name = "uuid", value = "User's UUID", dataType = "java.util.UUID", paramType = "path"),
 			@ApiImplicitParam(name = "SESSION_KEY", value = "User's session authentication key", dataType = "String", paramType = "header"),
 			@ApiImplicitParam(name = "type", value = "Type of membership to update", dataType = "String", paramType = "query", allowableValues = "assembly,group") })
+	@Dynamic(value = "OnlyMeAndAdmin", meta = SecurityModelConstants.USER_RESOURCE_PATH)
 	public static Result findMembershipByUser(Long uid, String type) {
 		User u = User.findByUserId(uid);
 		if (u == null)
