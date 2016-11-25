a6a16089b9ac1f57a633468438b18785621a1d7c
Cristhian Parra <cdparra@gmail.com>
2016-11-25 10:14:54 -0800

    Quick fix to #229

---
+++ conf/evolutions/default/9.sql
@@ -0,0 +1,9 @@
+-- Adding ON DELETE CASCADE to items in resource spaces to be able to delete them and their associations 
+-- to the resource space without problems and without deleting the resource space itself
+ALTER TABLE resource_space_resource
+  DROP CONSTRAINT fk_resource_space_resource_re_02;
+
+ALTER TABLE resource_space_resource
+  ADD CONSTRAINT fk_resource_space_resource_re_02 FOREIGN KEY (resource_resource_id)
+      REFERENCES resource (resource_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE CASCADE;
