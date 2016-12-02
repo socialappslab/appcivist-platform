e5ef470586c2f84ca40f3897fb520bead17c97f7
Cristhian Parra <cdparra@gmail.com>
2016-12-01 21:19:42 -0800

    Simplified version of password generator

--- test/TestingPasswordGenerator.java
+++ test/TestingPasswordGenerator.java
@@ -1,7 +1,15 @@
+import java.io.IOException;
 import java.math.BigInteger;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.SecureRandom;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import providers.MyLoginUsernamePasswordAuthUser;
 
@@ -10,74 +18,38 @@ public class TestingPasswordGenerator {
 	private static SecureRandom secureRandom = new SecureRandom();
 	
 	public static void main(String[] args) {
-		String[] emails = { 
-				"carmen@example.com",
-				"jane@example.com",
-				"jeff@example.com",
-				"sara@example.com",
-				"alicia@example.com",
-				"ted@example.com",
-				"leia@example.com",
-				"luke@example.com", 
-				"ahsoka@example.com", 
-				"bob@exampe.com"};
-		
-		String[] passwords = { "sip", "hop", "vouch", "value", "walk",
+		try {		
+			HashMap<String, String> emailPass = new HashMap<>();
+			String filedir = args[0];
+			String filename = args[1];
+			Path path = Paths.get(filedir + "/" + filename);
+			Files.lines(path)
+				.onClose(() -> System.out.println("File closed"))
+				.filter(s -> s.contains("@"))
+				.forEach((s) -> {
+					generatePassword(s, emailPass);
+				});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void generatePassword(String email, HashMap<String, String> emailPass) {
+		String[] seedPasswords = { "sip", "hop", "vouch", "value", "walk",
 				"act", "stray", "call", "curl", "stay", "yell", "tack",
 				"sing", "yawn", "open", "read", "edit",
 				"spell", "fix", "love", "knit", "like",
 				"praise", "hide", "clip" };
-
-		HashMap<String, String> emailPass = new HashMap<>();
-		
-		for (String email : emails) {
-			Integer passIndex = random.nextInt(25);
-			String pass = passwords[passIndex] + nextPassword();
-			MyLoginUsernamePasswordAuthUser authUser = 
-					new MyLoginUsernamePasswordAuthUser(pass, email);
-			// Hash a password for the first time
-			String hashed = authUser.getHashedPassword();
-			System.out.println(email+", clear=" + pass+", hashed="+hashed);
-			emailPass.put(email, hashed);
-		}
+		Integer passIndex = random.nextInt(25);
+		String pass = seedPasswords[passIndex] + nextPassword();
 		MyLoginUsernamePasswordAuthUser authUser = 
-				new MyLoginUsernamePasswordAuthUser("secret", "carmen@example.com");
+				new MyLoginUsernamePasswordAuthUser(pass, email);
 		// Hash a password for the first time
 		String hashed = authUser.getHashedPassword();
-		
-		String[] adminGmails = {
-				"appcivistapp+EN_ADMIN@gmail.com", 
-				"appcivistapp+ES_ADMIN@gmail.com", 
-				"appcivistapp+FR_ADMIN@gmail.com", 
-				"appcivistapp+DE_ADMIN@gmail.com", 
-				"appcivistapp+IT_ADMIN@gmail.com"};
-	
-		for (String email : adminGmails) {
-			String pass = "a44@c1v1st.2017." + nextPassword();
-			authUser = new MyLoginUsernamePasswordAuthUser(pass, email);
-			// Hash a password for the first time
-			hashed = authUser.getHashedPassword();
-			System.out.println(email+", clear=" + pass+", hashed="+hashed);
-			emailPass.put(email, hashed);
-		}
-		
-		String[] gmails = {
-				"appcivistapp+EN@gmail.com", 
-				"appcivistapp+ES@gmail.com", 
-				"appcivistapp+FR@gmail.com", 
-				"appcivistapp+DE@gmail.com", 
-				"appcivistapp+IT@gmail.com"};
-		
-		for (String email : gmails) {
-			String pass = "appcivist.2017" + nextPassword();
-			authUser = new MyLoginUsernamePasswordAuthUser(pass, email);
-			// Hash a password for the first time
-			hashed = authUser.getHashedPassword();
-			System.out.println(email+", clear=" + pass+", hashed="+hashed);
-			emailPass.put(email, hashed);
-		}
+		System.out.println(email+", clear=" + pass+", hashed="+hashed);
+		emailPass.put(pass, hashed);
 	}
-	
+
 	public static String nextPassword() {
 		return new BigInteger(20, secureRandom).toString(32);
 	}
