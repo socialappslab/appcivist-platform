c15ec818e2e28311724cad05a7b95e56a6faecce
Cristhian Parra <cdparra@gmail.com>
2017-02-21 20:23:14 -0300

    Made null-safe operations

--- app/models/User.java
+++ app/models/User.java
@@ -224,7 +224,7 @@ public class User extends AppCivistBaseModel implements Subject {
 
 
 	public Boolean isEmailVerified() {
-		return emailVerified;
+		return emailVerified != null ? emailVerified : false;
 	}
 
 
@@ -234,7 +234,7 @@ public class User extends AppCivistBaseModel implements Subject {
 
 
 	public boolean isActive() {
-		return active;
+		return active != null ? active : false;
 	}
 
 
