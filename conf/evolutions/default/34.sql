c6a6f603f9977fe9766a4d728edc1d5232a5bb6f
Marcelo <marce.mmad@gmail.com>
2017-04-18 16:37:50 -0400

    add Facebook user's id, access token and token expiration to appcivist user

---
+++ conf/evolutions/default/34.sql
@@ -0,0 +1,3 @@
+alter table appcivist_user add column facebook_user_id varchar(20);
+alter table appcivist_user add column user_access_token varchar(256);
+alter table appcivist_user add column token_expires_in varchar(20);
