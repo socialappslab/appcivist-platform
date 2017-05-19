03a7c9b87ed0aa6a88604452bf01c6feab2c94e4
javierpf <javierppf@gmail.com>
2017-05-19 16:37:57 -0400

    Added to postgres script file

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -1633,3 +1633,55 @@ insert into contribution_non_member_author select contribution_id, non_member_au
 alter table non_member_author add column uuid varchar(40);
 
 ALTER TABLE non_member_author ALTER COLUMN uuid SET DEFAULT uuid_generate_v4();
+
+-- 38.sql
+CREATE TEXT SEARCH CONFIGURATION "en-us" ( COPY = english );
+ALTER TEXT SEARCH CONFIGURATION "en-us" ALTER MAPPING
+FOR hword, hword_part, word WITH unaccent, english_stem;
+
+CREATE TEXT SEARCH CONFIGURATION "es-es" ( COPY = spanish );
+ALTER TEXT SEARCH CONFIGURATION "es-es" ALTER MAPPING
+FOR hword, hword_part, word WITH unaccent, spanish_stem;
+
+CREATE TEXT SEARCH CONFIGURATION "it-it" ( COPY = italian );
+ALTER TEXT SEARCH CONFIGURATION "it-it" ALTER MAPPING
+FOR hword, hword_part, word WITH unaccent, italian_stem;
+
+CREATE TEXT SEARCH CONFIGURATION "fr-fr" ( COPY = french );
+ALTER TEXT SEARCH CONFIGURATION "fr-fr" ALTER MAPPING
+FOR hword, hword_part, word WITH unaccent, french_stem;
+
+CREATE OR REPLACE FUNCTION update_language_contribution()
+RETURNS void AS $$
+DECLARE
+ rec_contribution   RECORD;
+ cur_contributions CURSOR
+ FOR SELECT *
+ FROM contribution;
+BEGIN
+   -- Open the cursor
+   OPEN cur_contributions;
+
+   LOOP
+      FETCH cur_contributions INTO rec_contribution;
+    -- exit when no more row to fetch
+      EXIT WHEN NOT FOUND;
+      RAISE NOTICE '%', rec_contribution.contribution_id;
+
+	EXECUTE format('UPDATE contribution SET lang = (select u.language from appcivist_user u, contribution_appcivist_user cu ' ||
+      'where cu.contribution_contribution_id = $1 and cu.appcivist_user_user_id = u.user_id limit 1) where contribution_id = $1')
+      USING rec_contribution.contribution_id;
+   END LOOP;
+
+   CLOSE cur_contributions;
+
+END; $$
+
+LANGUAGE plpgsql;
+
+select update_language_contribution();
+
+UPDATE contribution SET lang = 'en' where lang is NULL;
+
+UPDATE contribution
+SET document = to_tsvector(contribution.lang::regconfig, unaccent(coalesce(title,'')) || ' ' || unaccent(coalesce(text,'')));
