ce29ae281debfa3cbc54199bc64eeedff5bf8ce6
Marcelo <marce.mmad@gmail.com>
2017-05-19 18:44:46 -0400

    New custom field definition for source_url field

--- conf/evolutions/default/35.sql
+++ conf/evolutions/default/35.sql
@@ -2,3 +2,4 @@ ALTER TABLE contribution ADD COLUMN source VARCHAR;
 ALTER TABLE contribution ADD COLUMN source_url TEXT;
 ALTER TABLE non_member_author ADD COLUMN source VARCHAR;
 ALTER TABLE non_member_author ADD COLUMN source_url TEXT;
+INSERT INTO custom_field_definition (name, entity_type, field_type) VALUES ('source_url', 'CONTRIBUTION', 'STRING');
