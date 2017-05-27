a87e5a9741ded9ce00bbb045f49000511238afb9
javierpf <javierppf@gmail.com>
2017-05-27 19:41:10 -0400

    Attribute logo added to campaign model refs socialappslab/appcivist-platform#522 Add Logo to campaign model

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -1707,3 +1707,7 @@ ALTER TABLE custom_field_value_option ADD CONSTRAINT fk_custom_field_value_optio
 -- 41.sql
 alter table campaign add column cover_resource_id bigint;
 alter table campaign add constraint fk_contribution_resource_cover foreign key (cover_resource_id) references resource (resource_id);
+
+-- 42.sql
+alter table campaign add column logo_resource_id bigint;
+alter table campaign add constraint fk_campaign_resource_logo foreign key (logo_resource_id) references resource (resource_id);
