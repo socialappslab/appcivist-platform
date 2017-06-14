8e0083a578cb8fc49f417ca6b8842ac74b760918
Atilio <atiliovg94@gmail.com>
2017-06-14 19:06:10 -0400

    Created tests

--- test/models/ContributionsTest.java
+++ test/models/ContributionsTest.java
@@ -1,160 +1,35 @@
 package models;
 
-import be.objectify.deadbolt.java.DeadboltModule;
-import be.objectify.deadbolt.java.cache.HandlerCache;
-import ch.qos.logback.core.net.SyslogOutputStream;
-import com.amazonaws.util.json.JSONObject;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import static or.junit.Assert.*;
+
 import org.junit.Test;
-import play.Application;
-import play.Logger;
-import play.Mode;
-import play.api.cache.CacheApi;
-import play.inject.guice.GuiceApplicationBuilder;
-import play.libs.Json;
-import play.test.FakeApplication;
-import static org.junit.Assert.*;
 
-import java.util.List;
+import static play.test.*;
+import static play.test.Helpers.*;
+import static org.fest.assertions.Assertions.*;
 
-import play.libs.F.Promise;
-import play.test.WithApplication;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.sql.Timestamp;
-import java.util.*;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import delegates.ContributionsDelegate;
-import enums.ContributionTypes;
 
 
 public class ContributionsTest extends WithApplication {
-    public void testUpdateContribution() {
-		Contribution c = Contribution.read(new Long(1));
-		System.out.println("Contribution: "+c.getTitle());
-		String oldTitle = c.getTitle();
-		c.setTitle("[NEWTITLE]"+c.getTitle());
-		c.update();
-		c.refresh();
-		System.out.println("Contribution: "+c.getTitle());
-		assertTrue(!c.getTitle().equals(oldTitle));
-		c.setTitle(oldTitle);
-		c.update();
-		c.refresh();
-		System.out.println("Contribution: "+c.getTitle());
-		assertTrue(c.getTitle().equals(oldTitle));
-    }
-
-	private static final String BASE_URL = "http://localhost:9000";
-
 	@Test
-	public void testAddAndDeleteThemeInContribution() {
-		Theme t = new Theme();
-		t.setTitle("THEME1");
-		t.setDescription("THEME TEST");
-		List<Theme> themes = new ArrayList<Theme>();
-		themes.add(t);
-		JsonNode jsonNodeArray = Json.newObject();
-		((ObjectNode)jsonNodeArray).put("themes", Json.toJson(themes));
-		Logger.info("json+++++"+jsonNodeArray.toString());
-		try {
-			JsonNode obj = Json.parse(makeRequest(
-                    BASE_URL+"/api/contribution/05ac4be4-9960-4975-a8b7-6de893c384f4/themes", "POST", jsonNodeArray));
-			String themeId = "";
-			if (obj.isArray()) {
-				for (JsonNode objNode : obj) {
-					String title =  objNode.get("title").asText();
-					String description=objNode.get("description").asText();
-					themeId = objNode.get("themeId").asText();
-					Logger.info("jsontitle+++++"+title);
-					Logger.info("jsondescription+++++"+description);
-					assertTrue(t.getTitle().equals(title));
-					assertTrue(t.getDescription().equals(description));
-				}
-			}
+	running(FakeApplication(inMemoryDatabase()),new Runnable(){
+		public void run(){
+			User creator;
 
-			JsonNode objDelete = Json.parse(makeRequest(
-					BASE_URL+"/api/contribution/05ac4be4-9960-4975-a8b7-6de893c384f4/themes/"+themeId, "DELETE", null));
-			Object themesStr =  objDelete.get("themes");
-			Logger.info("jsonthemes+++++"+themesStr);
-			assertTrue(themesStr==null);
+			String title="Testing for contribution Model";
+			String text="Testing the module";
+			ContributionTypes type=ContributionTypes.COMMENT;
+			Contribution contribution = new Contribution(creator,title,text,type);
 
-		} catch (Exception e) {
-			e.printStackTrace();
+			contribution.save();
+
+    		Contribution savedC=Contribution.find.byId(contribution.id);
+
+    		assertThat(savedC).isNotNull;
+    		/*assertThat(savedC.creator)*/
+    		assertThat(savedC.title).isEqualTo(title);
+    		assertThat(savedC.text).isEqualTo(text);
+    		/*assertThat(savedC.type)*/
 		}
-
-
-	}
-
-	public static String makeRequest(String myUrl,
-									 String httpMethod, JsonNode parameters) throws Exception {
-		URL url = null;
-		url = new URL(myUrl);
-		HttpURLConnection conn = null;
-		conn = (HttpURLConnection) url.openConnection();
-		conn.setDoInput(true);
-		conn.setRequestProperty("Content-Type", "application/json");
-		conn.setRequestProperty("SESSION_KEY", "89284ca8f53d8c4cc2f144e241e19aeab1cdf769-pa.u.exp=1487873601777&pa.p.id=password&pa.u.id=carmen%40example.com");
-		DataOutputStream dos = null;
-		conn.setRequestMethod(httpMethod);
-
-		if (Arrays.asList("POST", "PUT").contains(httpMethod)) {
-			String params = parameters.toString();
-			conn.setDoOutput(true);
-			dos = new DataOutputStream(conn.getOutputStream());
-			dos.writeBytes(params);
-			dos.flush();
-			dos.close();
-		}
-
-		int respCode = conn.getResponseCode();
-		if (respCode != 200 && respCode != 201) {
-			String error = inputStreamToString(conn.getErrorStream());
-			return error;
-		}
-		String inputString = inputStreamToString(conn.getInputStream());
-		return inputString;
-	}
-  
-	public static String inputStreamToString(InputStream is) throws Exception {
-		BufferedReader br = null;
-		StringBuilder sb = new StringBuilder();
-
-		String line;
-		br = new BufferedReader(new InputStreamReader(is));
-		while ((line = br.readLine()) != null) {
-			sb.append(line);
-		}
-		br.close();
-		return sb.toString(); 
-	}
-	
-	@Test
-	public void testCreateContribution() {
-		User u = User.findByUserId(1l);
-		Contribution c = Contribution.create(u, "Test Create Contribution", "testing", ContributionTypes.DISCUSSION);
-		c.update();
-		c.refresh();
-		System.out.println("Contribution: " + c.getTitle());
-		assertTrue(c.getTitle().equals("Test Create Contribution"));		
-	}
-	
-	@Test
-	public void testUpdateCommentCounters() {
-		System.out.println("Update Comment Counters");
-		List<Contribution> contributions = Contribution.findAllByContainingSpace(74l);
-        
-		for (Contribution c: contributions){
-    		ContributionsDelegate.resetParentCommentCountersToZero(c);
-    		ContributionsDelegate.resetChildrenCommentCountersToZero(c);
-		}
-	}
+	});
 }
---
+++ test/models/README.md
@@ -0,0 +1,86 @@
+# Testing UNit
+
+## Test Commands
+
+ **To run all tests, run ```test```
+ **To run only one test class, run testOnly followed by the name of the class i.e. ```testOnly my.namespace.MyTest```
+ **To run only the tests that have failed, run ```testQuick```
+ **To run tests continually, run a command with a tilde in front, i.e. ```~testQuick```
+ **To access test helpers such as FakeApplication in console, run ```test:console```
+
+-----
+
+For testing we dont want to work with the database if it is possible, it is easier to work with temporary data
+
+The libraries you have to import are:
+```
+import static play.test.*;
+```
+
+```
+import static play.test.Helpers.*;
+```
+
+This library helps us create a fake application with in memory database
+
+```
+import static org.fest.assertions.Assertions.*;
+```
+
+This library give us more asserts than the jUnit default library
+
+After that we have to run the fakeapplication with an inmemory database with the following:
+
+```
+running(FakeApplication(inMemoryDatabase()),new Runnable(){
+	@Override
+	public void run(){
+	
+	}
+});
+```
+
+The new runnable is the code that we are going to run and test, in this case we are testing a creation method so we first create a new variable 
+of the model we are testing, in this case contribution:
+
+```
+User creator;
+String title="Testing for contribution Model";
+String text="Testing the module";
+ContributionTypes type=ContributionTypes.COMMENT;
+Contribution contribution = new Contribution(creator,title,text,type);
+```
+
+After that we save our new contribution
+
+```
+contribution.save();
+```
+
+Now we retrieve the saved contribution from the database with the following:
+
+```
+Contribution savedC=Contribution.find.byId(contribution.id);
+```
+
+After that we proceed to check if it is not null:
+
+```
+assertThat(savedC).isNotNull;
+```
+
+And then we check if the values saved in the database are equal to the ones we have:
+
+```
+    		assertThat(savedC.title).isEqualTo(title);
+```
+
+
+
+
+
+
+
+
+
+
