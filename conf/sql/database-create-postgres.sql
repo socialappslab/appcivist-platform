a41eab12a48564afa86c85517a0882db7ae4deb1
Cristhian Parra <cdparra@gmail.com>
2017-09-07 18:12:09 -0400

    Added evolution that was missing in #615 + added new statuses for contributions

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -1958,3 +1958,13 @@ ALTER TABLE ballot
   ADD CONSTRAINT ck_ballot_votes_limit_meaning 
   CHECK (votes_limit_meaning::text = 
     ANY (ARRAY['SELECTIONS'::character varying, 'TOKENS'::character varying, 'RANGE'::character varying]));
+
+ALTER TABLE contribution DROP CONSTRAINT ck_contrinution_contrinbutoin_status ;
+
+ALTER TABLE contribution
+  ADD CONSTRAINT ck_contrinution_contrinbutoin_status 
+  CHECK (status::text = 
+    ANY (ARRAY['NEW'::character varying, 'DRAFT'::character varying, 'PUBLISHED'::character varying,
+                    'ARCHIVED'::character varying, 'EXCLUDED'::character varying, 'MODERATED'::character varying, 
+                    'INBALLOT'::character varying, 'SELECTED'::character varying]::text[]));
+    
