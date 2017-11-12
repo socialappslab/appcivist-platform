543c4cc923d2f6077b4af7343294f1b275b5f243
Gabriela Gaona <gaby.lorely@gmail.com>
2017-11-12 19:37:12 -0300

    Revert "Add get providers service"
    This reverts commit 0f2bc1e0d71ec595f86f7fe47b97f3453baaea2e.

--- app/controllers/AuthenticateLocal.java
+++ app/controllers/AuthenticateLocal.java
@@ -1,22 +1,10 @@
 package controllers;
 
-import com.feth.play.module.pa.providers.AuthProvider;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import models.Campaign;
-import models.transfer.TransferResponseStatus;
-import play.libs.Json;
 import play.mvc.*;
 import play.mvc.Result;
 import service.PlayAuthenticateLocal;
 import com.feth.play.module.pa.controllers.Authenticate;
 import http.Headers;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @With(Headers.class)
 public class AuthenticateLocal extends Authenticate {
@@ -28,21 +16,5 @@ public class AuthenticateLocal extends Authenticate {
 
 		final String payload = request().getQueryString(PAYLOAD_KEY);
 		return PlayAuthenticateLocal.handleAuthentication(provider, ctx(), payload);
-	}
-
-	@ApiOperation(httpMethod = "GET", responseContainer = "List", produces = "application/json", value = "List of authentication providers")
-	@ApiResponses(value = {@ApiResponse(code = 404, message = "No provider found", response = TransferResponseStatus.class)})
-	public static Result getProviders() {
-		List<Map<String, Object>> toRet = new ArrayList<>();
-		for (AuthProvider a : AuthProvider.Registry.getProviders()) {
-			Map<String, Object> provider = new HashMap<>();
-			provider.put("url", a.getUrl());
-			provider.put("key", a.getKey());
-			toRet.add(provider);
-		}
-		if(toRet.isEmpty()) {
-			return notFound();
-		}
-		return ok(Json.toJson(toRet));
 	}
 }
