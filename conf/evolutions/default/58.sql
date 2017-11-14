ecf024c0e328d63f9f7a5c447f14bdb8392f9b73
Gabriela Gaona <gaby.lorely@gmail.com>
2017-11-14 08:16:15 -0300

    Add creator in WorkingGroup
    Signed-off-by: Gabriela Gaona <gaby.lorely@gmail.com>

---
+++ conf/evolutions/default/58.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.working_group ADD COLUMN creator_user_id bigint;
+ALTER TABLE public.working_group ADD CONSTRAINT fk_creator_wk FOREIGN KEY (creator_user_id) REFERENCES public.appcivist_user (user_id) MATCH SIMPLE;
