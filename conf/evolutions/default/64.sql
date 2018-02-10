bb61a622d1f7b19ea23593dfd56ad3b5d0bb7264
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-02-10 19:45:31 -0300

    Add location to campaign
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

---
+++ conf/evolutions/default/64.sql
@@ -0,0 +1,8 @@
+ALTER TABLE "public"."campaign" ADD COLUMN location_location_id bigint;
+ALTER TABLE "public"."campaign" ADD CONSTRAINT fk_campaign_location FOREIGN KEY (location_location_id)
+      REFERENCES location (location_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
+CREATE INDEX ix_campaign_location
+  ON campaign
+  USING btree
+  (location_location_id);
--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -2274,3 +2274,12 @@ END;
 ALTER TABLE "public"."subscription" DROP COLUMN default_identity;
 ALTER TABLE "public"."subscription" ADD COLUMN default_identity text;
 
+--64.sql
+ALTER TABLE "public"."campaign" ADD COLUMN location_location_id bigint;
+ALTER TABLE "public"."campaign" ADD CONSTRAINT fk_campaign_location FOREIGN KEY (location_location_id)
+      REFERENCES location (location_id) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION;
+CREATE INDEX ix_campaign_location
+  ON campaign
+  USING btree
+  (location_location_id);
