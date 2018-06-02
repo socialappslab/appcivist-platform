e85a75d6b6cf32e434c092163aba95deab139f50
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2018-06-02 02:22:12 -0400

    Correct update sql
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -3160,4 +3160,4 @@ alter table contribution_non_member_author   add CONSTRAINT fk_contribution_non_
 -- 81.sql
 ALTER TABLE contribution ADD column creator_user_id bigint;
 ALTER TABLE contribution ADD constraint fk_contribution_user foreign key (creator_user_id) references appcivist_user (user_id);
-UPDATE contribution set creator_id = (select appcivist_user_user_id from contribution_appcivist_user where contribution_contribution_id = contribution_id limit 1);
+UPDATE contribution set creator_user_id = (select appcivist_user_user_id from contribution_appcivist_user where contribution_contribution_id = contribution_id limit 1);
