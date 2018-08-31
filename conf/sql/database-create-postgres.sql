7a956984b11f35f8014c508660e6cd56de006796
Yohanna Lisnichuk <ylisnichuk@pol.una.py>
2018-08-30 21:41:17 -0400

    Update database-create-postgres.sql

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -3289,14 +3289,17 @@ select contribution_contribution_id, appcivist_user_user_id from contribution_ap
 -- 88.sql
 
 CREATE OR REPLACE FUNCTION add_proposals_from_theme_to_wg(theme_id bigint, wg_id bigint) RETURNS void AS $$
-    DECLARE
+     DECLARE
 
-    BEGIN
-      insert into resource_space_working_groups
-        select resource_space_resource_space_id, wg_id from contribution where resource_space_resource_space_id in (
-        select distinct resource_space_resource_space_id
-        from resource_space_theme
-        where theme_theme_id = theme_id
-      ) and type = 4;
-    END;
-$$ LANGUAGE plpgsql;
+     BEGIN
+       insert into resource_space_working_groups
+         select resource_space_resource_space_id, wg_id from contribution where resource_space_resource_space_id in (
+         select distinct resource_space_resource_space_id
+         from resource_space_theme
+         where theme_theme_id = theme_id
+       ) and type = 4 and
+            NOT EXISTS ( select rs.working_group_group_id from
+       resource_space_working_groups rs where rs.resource_space_resource_space_id = resource_space_resource_space_id
+            and rs.working_group_group_id = wg_id);
+     END;
+ $$ LANGUAGE plpgsql;
