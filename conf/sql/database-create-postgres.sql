58c4221818a9e39a7bd255e3824bb09d67c22ce3
Cristhian Parra <cdparra@gmail.com>
2019-08-08 18:37:37 -0400

    Updated sql creation script

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -3494,3 +3494,66 @@ ANY (ARRAY['NEW'::character varying::text, 'DRAFT'::character varying::text, 'PU
 'MODERATED'::character varying::text, 'INBALLOT'::character varying::text, 'SELECTED'::character varying::text,
 'FORKED_PRIVATE_DRAFT'::character varying::text, 'FORKED_PUBLIC_DRAFT'::character varying::text,
 'FORKED_PUBLISHED'::character varying::text, 'MERGED'::character varying::text]));
+
+-- 99.sql
+insert into  subscription
+    (space_type, subscription_type, user_id, space_id)
+    (
+        select distinct 'CONTRIBUTION',
+                        'REGULAR',
+                        au.uuid             as user_id,
+                        resource_space.uuid as resource_space_id
+        from contribution
+                 join resource_space on contribution.resource_space_resource_space_id = resource_space.resource_space_id
+                 join resource_space_working_groups rswg
+                      on contribution.resource_space_resource_space_id = rswg.resource_space_resource_space_id
+                 join membership on rswg.working_group_group_id = membership.working_group_group_id
+                 join appcivist_user au on membership.user_user_id = au.user_id
+        where (contribution.status = 'PUBLISHED'
+            or contribution.status = 'PUBLIC_DRAFT')
+          and contribution.removed is false
+    );
+
+-- 100.sql 
+CREATE OR REPLACE FUNCTION generate_proposal_source_codes
+  (campaignID bigint, replace_existing_codes boolean) RETURNS void AS $$
+DECLARE
+
+BEGIN
+  raise notice 'Generating source codes for campaig = % ...', campaignID;
+
+  IF replace_existing_codes THEN
+    UPDATE contribution co
+    SET source_code =  
+      CASE 
+          WHEN c.status in ('PUBLISHED', 'DRAFT', 'EXCLUDED', 'ARCHIVED', 'NEW', 'PUBLIC_DRAFT', 'INBALLOT') and c.parent_id is null then 'P'||c.contribution_id 
+          WHEN c.status not in ('FORKED_PUBLISHED') and c.parent_id is not null then 'P'||c.parent_id
+          ELSE 'P'||c.contribution_id||
+              substring('abcdefghijklmnopqrstuvwxyz'
+                from ( (select count(*) from contribution c2 where c2.contribution_id<c.contribution_id and c2.parent_id = c.parent_id and c2.status = 'FORKED_PUBLISHED')::integer + 1 ) 
+                  for 1
+              )
+      END
+    FROM contribution c
+    JOIN resource_space_contributions rsc on c.contribution_id = rsc.contribution_contribution_id 
+    JOIN campaign ca on rsc.resource_space_resource_space_id = ca.resources_resource_space_id 
+    WHERE ca.campaign_id = campaignID and co.contribution_id = c.contribution_id;
+  ELSE
+    UPDATE contribution co
+    SET source_code =  
+      CASE 
+          WHEN c.status in ('PUBLISHED', 'DRAFT', 'EXCLUDED', 'ARCHIVED', 'NEW', 'PUBLIC_DRAFT', 'INBALLOT') and c.parent_id is null then 'P'||c.contribution_id 
+          WHEN c.status not in ('FORKED_PUBLISHED') and c.parent_id is not null then 'P'||c.parent_id
+          ELSE 'P'||c.contribution_id||
+              substring('abcdefghijklmnopqrstuvwxyz'
+                from ( (select count(*) from contribution c2 where c2.contribution_id<c.contribution_id and c2.parent_id = c.parent_id and c2.status = 'FORKED_PUBLISHED')::integer + 1 ) 
+                  for 1
+              )
+      END
+    FROM contribution c
+    JOIN resource_space_contributions rsc on c.contribution_id = rsc.contribution_contribution_id 
+    JOIN campaign ca on rsc.resource_space_resource_space_id = ca.resources_resource_space_id 
+    WHERE ca.campaign_id = campaignID and co.contribution_id = c.contribution_id and c.source_code is null;
+  END IF;
+END;
+$$ LANGUAGE plpgsql;
