8aa73f4f285e7e5a5d5ef613fb9b6cd5044fa0b2
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-08-11 18:45:33 -0400

    Add database function sql
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- conf/sql/database-create-postgres.sql
+++ conf/sql/database-create-postgres.sql
@@ -3557,3 +3557,10 @@ BEGIN
   END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+-- 101.sql
+create or replace function naturalorder(text)
+    returns bytea language sql immutable strict as $f$
+    select string_agg(convert_to(coalesce(r[2], length(length(r[1])::text) || length(r[1])::text || r[1]), 'SQL_ASCII'),'\x00')
+    from regexp_matches($1, '0*([0-9]+)|([^0-9]+)', 'g') r;
+$f$;
