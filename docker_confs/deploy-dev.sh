3594b1865d243375ad0ceab92eedc753f577925a
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2019-10-07 15:55:40 -0300

    Add dev dockerfile
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

---
+++ docker_confs/deploy-dev.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /home/appcivist/production/appcivist-platform/conf/
+cp local.sample.conf local.conf
+cp play-authenticate/mine.sample.conf play-authenticate/mine.local.conf
+cp play-authenticate/smtp.sample.conf play-authenticate/smtp.local.conf
+cd /home/appcivist/production/appcivist-platform/
+
+
+./activator run
