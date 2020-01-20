dbf0d73f4983814052a8e00f81f26766feaf350a
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2020-01-20 00:45:59 -0300

    Update docker file
    Signed-off-by: Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>

--- docker_confs/deploy.sh
+++ docker_confs/deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd /home/appcivist/production/appcivist-platform/conf/
 source /environment
+mkdir -p /home/appcivist/.sbt
 cp local.sample.conf local.conf
 cp play-authenticate/mine.sample.conf play-authenticate/mine.local.conf
 cp play-authenticate/smtp.sample.conf play-authenticate/smtp.local.conf
 cd /home/appcivist/production/appcivist-platform/
-
+cp docker_confs/sbt /home/appcivist/.sbt/repositories
 
 ./activator stage && /etc/init.d/appcivist-backend start
